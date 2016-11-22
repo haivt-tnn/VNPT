64817c5f05cb159f0622d03289e43cc2b76474d8
hainguyentn94 <tienhai.ictu1004@gmail.com>
2016-11-22 20:39:11 +0700

    giao dien chua hoan thanh

---
+++ KhachSan/KhachSan/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhachSan.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
