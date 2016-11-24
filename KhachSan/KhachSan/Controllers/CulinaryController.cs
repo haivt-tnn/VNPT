94222a227e2185940e29a5c98114e0cac82054ce
hainguyentn94 <tienhai.ictu1004@gmail.com>
2016-11-24 09:12:03 +0700

    ban cap nhat thu 3

---
+++ KhachSan/KhachSan/Controllers/CulinaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhachSan.Controllers
+{
+    public class CulinaryController : Controller
+    {
+        // GET: Culinary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
