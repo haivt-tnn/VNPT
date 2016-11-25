5abcbd2ad0661e8fda5afb8fc7f08c5f5932c4fb
Pham Ba Viet <Pham Ba Viet>
2016-11-25 16:19:13 +0700

    vietup

--- KhachSan/KhachSan/Areas/Admin/AdminAreaRegistration.cs
+++ KhachSan/KhachSan/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace KhachSan.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index",controller="Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ KhachSan/KhachSan/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KhachSan.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public bool RememberMe { set; get; }
+    }
+}
