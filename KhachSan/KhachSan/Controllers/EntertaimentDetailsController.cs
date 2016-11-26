48e01e8e7cfa4791c3203a8a78e5092dae987d9e
hainguyentn94 <tienhai.ictu1004@gmail.com>
2016-11-26 11:45:08 +0700

    update giao dien nguoi dung

---
+++ KhachSan/KhachSan/Controllers/EntertaimentDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhachSan.Controllers
+{
+    public class EntertaimentDetailsController : Controller
+    {
+        // GET: EntertaimentDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KhachSan/KhachSan/Controllers/EventsDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhachSan.Controllers
+{
+    public class EventsDetailsController : Controller
+    {
+        // GET: EventsDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
