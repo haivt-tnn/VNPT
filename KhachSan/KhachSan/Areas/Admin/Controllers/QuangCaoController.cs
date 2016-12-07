454b0d93da26c835d5c572385df280a4644c55a8
Kien Nong <Kien Nong>
2016-12-07 15:56:17 +0700

    up

---
+++ KhachSan/KhachSan/Areas/Admin/Controllers/QuangCaoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhachSan.Areas.Admin.Controllers
+{
+    public class QuangCaoController : Controller
+    {
+        // GET: Admin/QuangCao
+        public ActionResult Index()
+        {
+            var t = new Models.AdsModel();
+            var model = t.getAllQC();
+            return View(model);
+        }
+    }
+}
